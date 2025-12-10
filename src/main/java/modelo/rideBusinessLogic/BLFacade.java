e075b9615e70c868d3bebd56f42d2bb7e0e48baa
Adrian476 <arodriguez476@ikasle.ehu.eus>
2025-12-10 14:57:10 +0100

    CAMBIO PAQUETE NOMBRE + PERSISTANCE

--- src/main/java/modelo/rideBusinessLogic/BLFacade.java
+++ src/main/java/modelo/rideBusinessLogic/BLFacade.java
@@ -1,4 +1,4 @@
-package modelo.businessLogic;
+package modelo.rideBusinessLogic;
 
 import java.util.Date;
 import java.util.List;
--- src/main/java/modelo/rideBusinessLogic/DataAccess.java
+++ src/main/java/modelo/rideBusinessLogic/DataAccess.java
@@ -1,4 +1,4 @@
-package modelo.businessLogic;
+package modelo.rideBusinessLogic;
 
 import java.io.File;
 import java.net.NoRouteToHostException;
