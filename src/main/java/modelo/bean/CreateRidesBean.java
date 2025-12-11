196a004aa9fafc3a11dab5547860b77792704130
ander <140650874+aaldanas@users.noreply.github.com>
2025-12-11 22:55:57 +0100

    Cambios realizados por shirley
    Co-Authored-By: shirley-11 <158266662+shirley-11@users.noreply.github.com>

--- src/main/java/modelo/bean/CreateRidesBean.java
+++ src/main/java/modelo/bean/CreateRidesBean.java
@@ -2,86 +2,104 @@ package modelo.bean;
 import java.io.Serializable;
 import java.util.*;
 import modelo.rideBusinessLogic.*;
-import modelo.rideDominio.Driver;
+
+import modelo.rideDominio.Ride;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
+@SuppressWarnings("serial")
 @Named("createRides")
 @SessionScoped
 public class CreateRidesBean implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
+
 	private String fromCity;
-    private String toCity;
-    private Date date; 
-    private String seats;
-    private String price;
-    private String driverEmail = "DriverTestJSF";
+	private String toCity;
+	private Date date; 
+	private String seats;
+	private String price;
+	private String driverEmail = "DriverTestJSF";
+	private String message;
 
-    private BLFacade facade;
-    
-    public CreateRidesBean() {
-    	this.facade = new BLFacadeImplementation();
-    }
-    
-    public String createRides() {
-    	try {
-    		
-    		facade.createRide(fromCity, toCity, date, Integer.parseInt(seats.trim()), Float.parseFloat(price.trim()), driverEmail);
-    		System.out.println("Viaje correctamente creado");
-    		return "viaje creado";
-    	} catch (Exception e){
-    		e.printStackTrace();
-    		return "error create";
-    	}
-    }
-    
-    public String getFromCity() { 
-    	return fromCity; 
-    }
 
-    public void setFromCity(String fromCity) { 
-    	this.fromCity = fromCity; 
-    }
 
-    public String getToCity() {
-    	return toCity; 
-    }
+	private BLFacade facade;
 
-    public void setToCity(String toCity) {
-    	this.toCity = toCity; 
-    }
+	public CreateRidesBean() {
+		this.facade = new BLFacadeImplementation();
+	}
 
-    public Date getDate() { 
-    	return date;
-    }
+	public String createRides() {
+		try {
 
-    public void setDate(Date date) { 
-    	this.date = date; 
-    }
+			Ride r = facade.createRide(fromCity, toCity, date, Integer.parseInt(seats.trim()), Float.parseFloat(price.trim()), driverEmail);
+			if (r != null) {
+				message = "Ride creado correctamente.";
+			} else {
+				message = "No se ha podido crear el ride.";
+			}
 
-    public String getSeats() { 
-    	return seats; 
-    }
+			return "viaje creado";
+		} catch (Exception e){
+			e.printStackTrace();
+			message = "error create";
+			return "error create";
+		}
+	}
 
-    public void setSeats(String seats) { 
-    	this.seats = seats; 
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    public String getPrice() { 
-    	return price; 
-    }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public void setPrice(String price) { 
-    	this.price = price; 
-    }
-    
-    public String getDriverEmail() {
-    	return driverEmail;
-    }
-    
-    public void setDriverEmail(String driverEmail) {
-    	this.driverEmail = driverEmail;
-    }
+	public String getFromCity() { 
+		return fromCity; 
+	}
+
+	public void setFromCity(String fromCity) { 
+		this.fromCity = fromCity; 
+	}
+
+	public String getToCity() {
+		return toCity; 
+	}
+
+	public void setToCity(String toCity) {
+		this.toCity = toCity; 
+	}
+
+	public Date getDate() { 
+		return date;
+	}
+
+	public void setDate(Date date) { 
+		this.date = date; 
+	}
+
+	public String getSeats() { 
+		return seats; 
+	}
+
+	public void setSeats(String seats) { 
+		this.seats = seats; 
+	}
+
+	public String getPrice() { 
+		return price; 
+	}
+
+	public void setPrice(String price) { 
+		this.price = price; 
+	}
+
+	public String getDriverEmail() {
+		return driverEmail;
+	}
+
+	public void setDriverEmail(String driverEmail) {
+		this.driverEmail = driverEmail;
+	}
 
 }
